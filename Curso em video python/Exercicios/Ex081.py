0eba6e3c8c9103dc87b5092d31665ee29a8dc148
Phsarmento <pedrohenriquesarmento@gmail.com>
2024-02-14 18:50:55 -0300

    Ex 081 e 082 - prontos

--- Curso em video python/Exercicios/Ex081.py
+++ Curso em video python/Exercicios/Ex081.py
@@ -2,3 +2,21 @@
 A) Quantos números foram digitados
 B) A lista de valores ordenada de forma descrescente
 C) Se o valor 5 foi digitado e está ou não na lista'''
+lista = []
+resp = 'a'
+n5 = 0
+while True:
+    num = int(input('Digite um número: '))
+    lista.append(num)
+    resp = input('Você deseja continuar? [S/N] ')
+    if resp.upper() == 'N':
+        break
+print(f'Nesta lista contem {len(lista)} números digitados!')
+print('Os valores desta lista em ordem decrescente é: ',end='')
+for c in range(0,len(lista)):
+    lista.sort(reverse=True)
+    print(lista[c],end=', ')
+    if lista[c] == 5:
+        n5 += 1
+print('')
+print(f'O número 5 aparece na lista {n5} veze(s)!')
--- Curso em video python/Exercicios/Ex082.py
+++ Curso em video python/Exercicios/Ex082.py
@@ -1,2 +1,19 @@
 '''Crie um programa que vai ler varios numeros e colocar em uma lista.
 Depois disso, cire duas listas extras que vão conter apenas os valores pares e os valores impares digitados, respectivamente. Ao final mostre o conteudo das tres listas geradas'''
+lista = []
+listap = []
+listai = []
+while True:
+    num = int(input('Digite um número: '))
+    lista.append(num)
+    resp = input('Você deseja continuar? [S/N] ')
+    if resp.upper() == 'N':
+        break
+for c in range (0,len(lista)):
+    if lista[c]%2 == 0:
+        listap.append(lista[c])
+    else:
+        listai.append(lista[c])
+print(f'Lista geral {lista}')
+print(f'Lista dos pares {listap}')
+print(f'Lista dos impares {listai}')
